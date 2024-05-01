e8554f630700a8ad353256cafbec22b1bb5e526a
Dominic <125dmiguelcruz@frhsd.com>
2024-05-01 10:55:55 -0400

    facts

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function updateOne(){
+    document.getElementById('output').innerHTML = "He was the youngest player to be drafted by the NBA number 1 at the age of 18.";
+}
+function updateTwo(){
+    document.getElementById('output').innerHTML = "LeBron has won two Olympic gold medals with Team USA basketball, in 2008 and 2012, showcasing his prowess on the international stage.";
+}
+function updateThree(){
+    document.getElementById('output').innerHTML = "LeBron is not just a basketball player; he's a businessman. He's invested in various business ventures, including a stake in Liverpool FC, a production company, and a deal with Nike for his signature shoe line.";
+}
